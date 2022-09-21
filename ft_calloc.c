8f24c49c066849ee9531d4352dfc4454a5624e89
Marius Winter <mariuskayawinter@gmail.com>
2022-09-21 17:32:17 +0200

    fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mawinter <mawinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 13:18:17 by mawinter          #+#    #+#             */
-/*   Updated: 2022/03/26 14:43:18 by mawinter         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:32:06 by mawinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	int		i;
 	void	*ptr;
 
-	i = 0;
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
